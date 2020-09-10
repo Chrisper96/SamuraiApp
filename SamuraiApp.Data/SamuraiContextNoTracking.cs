68b48ad7a9285cce32eed2316075db395bc3eaf4
Christos Perris Henriksen <chri658v@elev.tec.dk>
2020-09-10 12:31:02 +0200

    Opgave 5 finished.

---
+++ SamuraiApp.Data/SamuraiContextNoTracking.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SamuraiApp.Data
+{
+    class SamuraiContextNoTracking : DbContext
+    {
+        public SamuraiContextNoTracking()
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
+
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Clan> Clans { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+        public DbSet<Horse> Horses { get; set; }
+    }
+}
